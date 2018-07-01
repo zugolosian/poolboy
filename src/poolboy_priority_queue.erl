7765850cbe7a7c179fc8a49a05d78f1ef763dd75
David Leach <david.leach@fluxfederation.com>
2018-07-01 18:38:49 +1200

    More specific specs

--- src/poolboy_priority_queue.erl
+++ src/poolboy_priority_queue.erl
@@ -12,18 +12,20 @@
 %% API
 -export([new/0,put/3,get/1,get_with_strategy/2,get_r/1,peek/1,length/1,lookup_by_value/2,delete/2,delete_by_value/2,to_list/1]).
 
+-export_type([priority/0]).
+-type priority() :: integer() | {integer(), integer()}.
 -spec new() -> gb_trees:tree().
 
 new() ->
     gb_trees:empty().
 
--spec put(Priority :: integer() | tuple(), Item :: term(), Queue :: gb_trees:tree(_,_)) ->
+-spec put(Priority :: priority(), Item :: term(), Queue :: gb_trees:tree(_,_)) ->
     gb_trees:tree(_,_).
 
 put(Priority, Item, Queue) ->
     gb_trees:insert(Priority, Item, Queue).
 
--spec get(gb_trees:tree(_,_)) -> {'empty'} | {_,_,gb_trees:tree(_,_)}.
+-spec get(gb_trees:tree(_,_)) -> {'empty'} | gb_trees:tree(_,_).
 get(Queue) ->
     case gb_trees:is_empty(Queue) of
         false ->
@@ -32,7 +34,7 @@ get(Queue) ->
             {empty}
     end.
 
--spec get_r(gb_trees:tree(_,_)) -> {'empty'} | {_,_,gb_trees:tree(_,_)}.
+-spec get_r(gb_trees:tree(_,_)) -> {'empty'} | gb_trees:tree(_,_).
 get_r(Queue) ->
     case gb_trees:is_empty(Queue) of
         false ->
@@ -42,14 +44,15 @@ get_r(Queue) ->
     end.
 
 -spec get_with_strategy(Queue :: gb_trees:tree(_,_),'fifo' | 'lifo') ->
-    {'empty'} | {_,_,gb_trees:tree(_,_)}.
+    {'empty'} | gb_trees:tree(_,_).
 
 get_with_strategy(Queue, lifo) ->
     poolboy_priority_queue:get_r(Queue);
 get_with_strategy(Queue, fifo) ->
     poolboy_priority_queue:get(Queue).
 
--spec peek(gb_trees:tree(_,_)) -> {_,_}.
+-spec peek(Queue :: gb_trees:tree(_,_)) ->
+    {empty, gb_trees:tree()} | {Key :: priority(), Value :: term()}.
 
 peek(Queue) ->
     case gb_trees:is_empty(Queue) of
@@ -90,15 +93,21 @@ length(Queue) ->
 to_list(Queue) ->
     gb_trees:to_list(Queue).
 
--spec lookup_by_value(_,'none' | {_,_,_} | gb_trees:tree(_,_)) -> {_}.
-lookup_by_value(_Value, none) ->
-    {none};
-lookup_by_value(Value, {Key, Value, _Iterable}) ->
-    {Key};
-lookup_by_value(Value, {_Key, _Value, Iterable}) ->
-    lookup_by_value(Value, gb_trees:next(Iterable));
+-spec lookup_by_value(Values :: pid(), Queue :: gb_trees:tree()) ->
+
+    {none} | {priority()}.
 lookup_by_value(Value, Queue) ->
-    lookup_by_value(Value, gb_trees:next(gb_trees:iterator(Queue))).
+    lookup_by_value_iter(Value, gb_trees:next(gb_trees:iterator(Queue))).
+
+-spec lookup_by_value_iter(Value :: pid(), none | gb_trees:iter()) ->
+    {none} | {priority()}.
+lookup_by_value_iter(_Value, none) ->
+    {none};
+lookup_by_value_iter(Value, {Key, Value, _Iterable}) ->
+    {Key};
+lookup_by_value_iter(Value, {_Key, _Value, Iterable}) ->
+    lookup_by_value_iter(Value, gb_trees:next(Iterable)).
+
 
 
 
